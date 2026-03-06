a47ab5bed06ef95c7643d42bbb06207a1ce5c923
prajapatinaveen <prajapatinaveen9719@gmail.com>
2026-03-06 20:54:24 +0530

    deploy test

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace MonitoringStack.Controllers
         [HttpGet]
         public IActionResult GetHello()
         {
-            return Ok("Hello World");
+            return Ok("Hello World from ci cd pipeline !git add .\r\n");
         }
 
 
